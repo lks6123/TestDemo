41249b405870827c3722ec4269b3365e8b1846f9
李露 <lks6123@163.com>
2017-02-23 02:31:34 +0800

    小错误修改

--- app/src/main/java/com/shao/testdemo/MainActivity.java
+++ app/src/main/java/com/shao/testdemo/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private ArrayList<Fragment> fragments;
     private ArrayList<TextView> textViewList;
+    //被选中的TextView在textViewList中的位置
     private int selected;
     private FragmentManager mFragmentManager;
 
@@ -44,7 +45,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public int getContentViewID() {
         return R.layout.activity_main;
     }
-
+    //遍历TextView集合，将选中的TextView设置为未选中状态
     public void unSelectedAll() {
         for (TextView textView : textViewList) {
             if (textView.isSelected()) {
@@ -55,12 +56,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
+        //初始化数据
         initData();
-        //防止旋转屏幕之后重复加载
         setDefaultFragment();
         initTextViewClickListener();
     }
-
+    //初始化所有的textView的单击监听
     private void initTextViewClickListener() {
         tabDeal.setOnClickListener(this);
         tabMore.setOnClickListener(this);
@@ -80,6 +81,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         fragments.add(TwoFragment.newInstance("FragmentTwo"));
         fragments.add(ThreeFragment.newInstance("FragmentThree"));
         fragments.add(FourFragment.newInstance("FragmentFour"));
+        //初始化FragmentManager
         if (mFragmentManager == null){
             mFragmentManager = getSupportFragmentManager();
         }
@@ -95,18 +97,27 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         textViewList.add(tabMore);
     }
 
-
+    //初始化刚进入时的Fragment
     private void setDefaultFragment() {
         Log.d(TAG_LOG,"加载默认的Fragment");
         Fragment fragment = mFragmentManager.findFragmentById(R.id.fram_main_activity);
+        //防止旋转屏幕之后重复加载
         if(fragment == null ) {
             tabDeal.setSelected(true);
             fragment = fragments.get(0);
             mFragmentManager.beginTransaction().add(R.id.fram_main_activity,fragment).commit();
-            Log.d(TAG_LOG,"防止旋转屏幕之后重复加载");
+            Log.d(TAG_LOG,"检查屏幕是否重复加载");
         }
     }
 
+    //保存屏幕旋转时被选中的TextView在List中的位置，以便Activity重建之后知道恢复TextView被选中状态
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_TEXT_VIEW,selected);
+    }
+
+    //恢复被选中的TextView状态
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -118,11 +129,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(SELECTED_TEXT_VIEW,selected);
-    }
 
     @Override
     public void onClick(View v) {
@@ -142,6 +148,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
+    //切换Fragment
     private void switchFragment(int index) {
         unSelectedAll();
         textViewList.get(index).setSelected(true);
